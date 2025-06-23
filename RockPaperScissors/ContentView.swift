38c02aecf6443302a9cf9d134964402c58b24506
Havva Odabaşı <havvaodabasi@MacBookPro.modem.turktelekom>
2025-06-23 17:50:33 +0300

    Added choice buttons, game logic and scoring

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -21,8 +21,45 @@ struct ContentView: View {
             Text("App chose: \(moves[appMove])")
             Text(shouldWin ? "You need to WIN" : "You need to LOSE")
         }
+        HStack {
+            ForEach(0..<3) { number in
+                Button {
+                    // henüz fonksiyon yok
+                } label: {
+                    Text(moves[number])
+                        .font(.system(size: 60))
+                }
+            }
+        }
     }
+    
+    func tappedMove(_ playerMove: Int) {
+        if checkAnswer(playerMove) {
+            score += 1
+        } else {
+            score -= 1
+        }
+        
+        nextRound()
+    }
+
+    func checkAnswer(_ playerMove: Int) -> Bool {
+        if shouldWin {
+            return playerMove == (appMove + 1) % 3
+        } else {
+            return playerMove == (appMove + 2) % 3
+        }
+    }
+
+    func nextRound() {
+        appMove = Int.random(in: 0...2)
+        shouldWin.toggle()
+        round += 1
+    }
+
 }
+
+
 
 #Preview {
     ContentView()
